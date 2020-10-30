588bfd46f1dfd47ec690cc104d92e94454463603
unknown <xiongjoh@gmail.com>
2020-10-30 11:48:49 -0500

    add data-testid to class plant-card

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -31,7 +31,7 @@ export default class PlantList extends Component {
     return (
       <main className="plant-list">
         {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id}>
+          <div data-testid="plant-card" className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
               <h2 className="plant-name">{plant.name}</h2>
